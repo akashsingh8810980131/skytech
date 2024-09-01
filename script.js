651325cfeedd8288b9e619a3cf995fcee43c8b3b
akashsingh8810980131 <121671231+akashsingh8810980131@users.noreply.github.com>
2024-09-01 21:42:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-document.getElementById('fetchButton').addEventListener('click', fetchUserName);
-
-function fetchUserName() {
-  const userId = document.getElementById('userIdInput').value;
-  fetch('https://raw.githubusercontent.com/akashsingh8810980131/skytech/main/data.json')
-    .then(response => response.json())
-    .then(data => {
-      const user = data.find(user => user.id === parseInt(userId));
-      const resultDiv = document.getElementById('result');
-      if (user) {
-        resultDiv.innerHTML = `<p>User Name: ${user.name}</p>`;
-      } else {
-        resultDiv.innerHTML = '<p>User not found.</p>';
-      }
-    })
-    .catch(error => {
-      console.error('Error fetching data:', error);
-      document.getElementById('result').innerHTML = '<p>Error fetching data. Please try again.</p>';
-    });
+function myFunction() {
+    document.getElementById("demo").innerHTML="Paragraph changed.";
 }
